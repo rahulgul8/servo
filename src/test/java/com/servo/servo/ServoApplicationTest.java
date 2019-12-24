f6585226c8ccb07be7d4201e0a8606adf48fcb28
rahulgul8 <rahulgul8@gmail.com>
2019-12-24 19:09:49 +0530

    Create ServoApplicationTest.java

---
+++ src/test/java/com/servo/servo/ServoApplicationTest.java
@@ -0,0 +1,13 @@
+package com.servo.servo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
