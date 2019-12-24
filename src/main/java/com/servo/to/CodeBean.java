2666916b56c36b43c1ec161741a6e25ccd8df683
rahulgul8 <rahulgul8@gmail.com>
2019-12-24 19:07:31 +0530

    Create CodeBean.java

---
+++ src/main/java/com/servo/to/CodeBean.java
@@ -0,0 +1,18 @@
+package com.servo.to;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CodeBean {
+
+    private String className;
+
+    private String code;
+
+}
