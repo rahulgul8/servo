f7ae88fba9d55cd053d2bda2fb95fcdc86794a04
rahulgul8 <rahulgul8@gmail.com>
2019-12-24 18:59:13 +0530

    Create ServoApplication.java

---
+++ src/main/java/com/servo/ServoApplication.java
@@ -0,0 +1,13 @@
+package com.servo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServoApplication.class, args);
+    }
+
+}
